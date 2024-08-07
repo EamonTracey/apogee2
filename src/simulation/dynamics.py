fc90e1af6fb989715951fd6460a4377115a9b8e5
eamontracey <etracey@nd.edu>
2024-08-06 20:52:39 -0700

    Update state handling

--- src/simulation/dynamics.py
+++ src/simulation/dynamics.py
@@ -1,6 +1,19 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import numpy as np
+
 from base.constants import EARTH_GRAVITY_ACCELERATION, SPEED_OF_SOUND
 from simulation.motor import Motor
 from simulation.vehicle import Vehicle
+
+
+@dataclass
+class SimulationState:
+    time: float = 0.0
+
+    position: tuple[float, float, float] = (0.0, 0.0, 0.0)
+    velocity: tuple[float, float, float] = (0.0, 0.0, 0.0)
 
 
 class DynamicsSimulation:
@@ -9,54 +22,36 @@ class DynamicsSimulation:
         self._vehicle = vehicle
         self._motor = motor
 
-        self.initialize_conditions()
+        self._state = SimulationState()
 
     @property
-    def time(self):
-        return self._time
-
-    @property
-    def vehicle_position(self):
-        return self._vehicle_position
-
-    @property
-    def vehicle_velocity(self):
-        return self._vehicle_velocity
-
-    @property
-    def vehicle_acceleration(self):
-        return self._vehicle_acceleration
-
-    def initialize_conditions(self,
-                              vehicle_position: float = 0,
-                              vehicle_velocity: float = 0,
-                              ignition_time: float = 0):
-        self._time = 0
-        self._vehicle_acceleration = 0
-
-        self._vehicle_position = vehicle_position
-        self._vehicle_velocity = vehicle_velocity
-        self._ignition_time = ignition_time
+    def state(self):
+        return self._state
 
     def step(self, time_delta: float):
         assert time_delta > 0
 
+        # Unpack the simulation state.
+        time = self.state.time
+        position = np.array(self.state.position)
+        velocity = np.array(self.state.velocity)
+
         # Derive mach number.
-        mach = self._vehicle_velocity / SPEED_OF_SOUND
+        mach = velocity[2] / SPEED_OF_SOUND
 
         # Calculate the forces acting on the rocket.
-        mass_total = self._vehicle.mass + self._motor.calculate_mass(
-            self._time - self._ignition_time)
+        mass_total = self._vehicle.mass + self._motor.calculate_mass(time)
         force_gravity = mass_total * -EARTH_GRAVITY_ACCELERATION
-        force_thrust = self._motor.calculate_thrust(self._time -
-                                                    self._ignition_time)
+        force_thrust = self._motor.calculate_thrust(time)
         force_drag = -self._vehicle.calculate_drag(mach)
         force_total = force_gravity + force_thrust + force_drag
 
         # Advance Newtonian physics.
-        self._vehicle_acceleration = force_total / mass_total
-        self._vehicle_velocity += self._vehicle_acceleration * time_delta
-        self._vehicle_position += self._vehicle_velocity * time_delta
+        acceleration = force_total / mass_total
+        velocity[2] += acceleration * time_delta
+        position[2] += velocity[2] * time_delta
 
-        # Update time.
-        self._time += time_delta
+        # Repack the simulation state.
+        self._state.time = time + time_delta
+        self._state.position = tuple(position)
+        self._state.velocity = tuple(velocity)
