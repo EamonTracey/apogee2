0ee2eefcf27dbbb1c18416069fa43f9d415dfd55
eamontracey <etracey@nd.edu>
2024-08-16 16:37:48 -0400

    Create CFD script

--- src/scripts/apogee.py
+++ src/scripts/apogee.py
@@ -1,8 +1,11 @@
 import click
+from trogon import tui
 
+from scripts.cfd import cfd
 from scripts.simulate import simulate
 
 
+@tui()
 @click.group()
 def apogee():
     """A suite of software tools for the 2024-25 Apogee Control System.
@@ -11,6 +14,7 @@ def apogee():
     pass
 
 
+apogee.add_command(cfd)
 apogee.add_command(simulate)
 
 if __name__ == "__main__":
