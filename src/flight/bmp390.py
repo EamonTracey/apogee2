df4e2b20809155e7980787a6d7ec665106b4e457
eamontracey <etracey@nd.edu>
2024-11-03 17:50:38 -0500

    small changes

--- src/flight/bmp390.py
+++ src/flight/bmp390.py
@@ -2,13 +2,16 @@ from dataclasses import dataclass
 
 from base.component import Component
 
+
 @dataclass
 class BMP390State:
     pressure: float = 0
     altitude: float = 0
     temperature: float = 0
 
+
 class BMP390Component(Component):
+
     def __init__(self):
         self._state = BMP390State()
 
--- src/flight/bno085.py
+++ src/flight/bno085.py
@@ -12,17 +12,23 @@ import numpy as np
 
 @dataclass
 class BNO085State:
+    # Acceleration in meters per second squared.
     acceleration: tuple[float, float, float] = (0, 0, 0)
+
+    # Magnetic field in microteslas.
     magnetic: tuple[float, float, float] = (0, 0, 0)
-    euler: tuple[float, float, float] = (0, 0, 0)
+
+    # Angular velocity radians per second.
     gyro: tuple[float, float, float] = (0, 0, 0)
+
+    # Orientation as a (w, x, y, z) quaternion.
     quaternion: tuple[float, float, float, float] = (0, 0, 0, 0)
 
-    acceleration_readings: int = 0
-    magnetic_readings: int = 0
-    euler_readings: int = 0
-    gyro_readings: int = 0
-    quaternion_readings: int = 0
+    # Count the number of times each reading fails.
+    acceleration_errors: int = 0
+    magnetic_errors: int = 0
+    gyro_errors: int = 0
+    quaternion_errors: int = 0
 
 
 class BNO085Component(Component):
@@ -43,71 +49,50 @@ class BNO085Component(Component):
         return self._state
 
     def dispatch(self):
-        #getting data
+        # Read the raw acceleration.
+        acceleration = None
         try:
-            (acc_x, acc_y, acc_z) = self.sensor.acceleration()
-            (gyro_x, gyro_y, gyro_z) = self.sensor.gyro()
-            #(euler_x, euler_y, euler_z) = self.sensor.euler()
-            (mag_x, mag_y, mag_z) = self.sensor.magnetic()
-            (quaternion_w, quaternion_x, quaternion_y, quaternion_z) = self.sensor.quaternion()
+            acceleration = self._bno085.acceleration
+        except Exception as exception:
+            # TODO: Implement logging.
+            ...
+        if acceleration is not None:
+            self._state.acceleration = acceleration
+        else:
+            self._state.acceleration_errors += 1
 
-            #writing if valid
-            if (acc_x and acc_y and acc_z):
-                self._state.acceleration = (acc_x, acc_y, acc_z)
-                self._state.acceleration_readings += 1
+        # Read the raw magnetic field.
+        magnetic = None
+        try:
+            magnetic = self._bno085.magnetic
+        except Exception as exception:
+            # TODO: Implement logging.
+            ...
+        if magnetic is not None:
+            self._state.magnetic = magnetic
+        else:
+            self._state.magnetic_errors += 1
 
-            if (gyro_x and gyro_y and gyro_z):
-                self._state.gyro = (gyro_x, gyro_y, gyro_z)
-                self._state.gyro_readings += 1
+        # Read the raw angular velocity.
+        gyro = None
+        try:
+            gyro = self._bno085.gyro
+        except Exception as exception:
+            # TODO: Implement logging.
+            ...
+        if gyro is not None:
+            self._state.gyro = gyro
+        else:
+            self._state.gyro_errors += 1
 
-            #not reading euler because can calculate it from quaternion
-            """
-            if (euler_x and euler_y and euler_z):
-                self._state.euler = (euler_x, euler_y, euler_z)
-                self._state.euler += 1
-            """
-
-            if (mag_x and mag_y and mag_z):
-                self._state.magnetic = (mag_x, mag_y, mag_z)
-                self._state.magnetic_readings += 1
-
-            if (quaternion_x and quaternion_y and quaternion_z):
-                self._state.quaternion = (quaternion_w, quaternion_x, quaternion_y, quaternion_z)
-                #euler angle from quaternion!
-                self._state.euler = quatern2euler((quaternion_w, quaternion_x, quaternion_y, quaternion_z))
-                self._state.quaternion_readings += 1
-
-        except Exception as e:
-            print(f'Exception has occured: {e} | {datetime.now().time()}')
-            
-
-
-
-def quatern2euler(quaternion_w, quaternion_x, quaternion_y, quaternion_z):
-    """
-    Converts a quaternion orientation to ZYX Euler angles.
-    
-    Parameters:
-    q : numpy array of shape (N, 4)
-        Array of quaternions, where each quaternion is represented as [qw, qx, qy, qz].
-
-    Returns:
-    euler : numpy array of shape (N, 3)
-        Array of Euler angles [phi, theta, psi] for each quaternion.
-
-    COPIED FROM ZEBNERS MATLAB CODE WHICH IS BASED OFF WIKI for rotation matrix
-    """
-
-    # Precompute elements of the rotation matrix
-    R11 = 2 * quaternion_w**2 - 1 + 2 * quaternion_x**2
-    R21 = 2 * (quaternion_x * quaternion_y - quaternion_w * quaternion_z)
-    R31 = 2 * (quaternion_x * quaternion_z + quaternion_w * quaternion_y)
-    R32 = 2 * (quaternion_y * quaternion_z - quaternion_w * quaternion_x)
-    R33 = 2 * quaternion_w**2 - 1 + 2 * quaternion_z**2
-
-    # Compute Euler angles
-    phi = np.arctan2(R32, R33)
-    theta = -np.arctan(R31 / np.sqrt(1 - R31**2))
-    psi = np.arctan2(R21, R11)
-
-    return (phi, theta, psi)
+        # Read the fused orientation.
+        quaternion = None
+        try:
+            quaternion = self._bno085.quaternion
+        except Exception as exception:
+            # TODO: Implement logging.
+            ...
+        if quaternion is not None:
+            self._state.quaternion = quaternion
+        else:
+            self._state.quaternion_errors += 1
