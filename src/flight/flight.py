e896060a9a3e1f18a362e7e1e92de182ed8fdaf9
eamontracey <etracey@nd.edu>
2024-11-03 17:19:00 -0500

    log file path and more

--- src/flight/flight.py
+++ src/flight/flight.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Optional
 
 import board
 import busio
@@ -6,19 +7,24 @@ import busio
 from base.component import Component
 from base.loop import Loop
 from flight.bmp390 import BMP390Component
+from flight.bno085 import BNO085Component
 from flight.icm20649 import ICM20649Component
 from flight.log import LogComponent
 
 
 class Flight:
 
-    def __init__(self):
+    def __init__(self, log_file: Optional[str] = None):
         self.loop = Loop(30)
 
         # Naming is hard.
         utc_date = datetime.datetime.now(datetime.UTC)
         utc_date_string = strftime("%Y%m%d%H%M%S")
         name = f"ACS {utc_date_string}"
+
+        # Handle optional configurations.
+        if log_file is None:
+            log_file = f"{name}.log"
 
         # Connect to the I2C bus.
         i2c = busio.I2C(board.SCL, board.SDA)
@@ -28,10 +34,10 @@ class Flight:
         bmp390_state = bmp390_component.state
         self.loop.add_component(bmp390_component, 30)
 
-        # BNO055.
-        bno055_component = BNO055Component(i2c)
-        bno055_state = bno055_component.state
-        self.loop.add_component(bno055_component, 30)
+        # BNO085.
+        bno085_component = BNO085Component(i2c)
+        bno085_state = bno085_component.state
+        self.loop.add_component(bno085_component, 30)
 
         # ICM20649.
         icm20649_component = ICM20649Component(i2c)
@@ -39,9 +45,9 @@ class Flight:
         self.loop.add_component(icm20649_component, 30)
 
         # Log.
-        log_component = LogComponent(f"{name}.log", bmp390_state, bno055_state,
+        log_component = LogComponent(log_file, bmp390_state, bno085_state,
                                      icm20649_state)
         self.loop.add_component(log_component, 30)
 
-    def fly(self):
+    def run(self):
         self.loop.run(0)
--- src/scripts/fly.py
+++ src/scripts/fly.py
@@ -1,11 +1,20 @@
+from typing import Optional
+
 import click
 
 from flight.flight import Flight
 
 
 @click.command(context_settings={"show_default": True})
-def fly():
+@click.option("log_file",
+              type=Optional[str],
+              default=None,
+              help="The path to which to write the log file")
+def fly(log_file: Optional[str]):
     """Run ACS flight software."""
+
+    flight = Flight(log_file)
+    flight.run()
 
 
 if __name__ == "__main__":
