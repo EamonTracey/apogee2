7b5ddcb7a185fd0a205b5da5d517670ecb2e4d71
Eamon Tracey <etracey@nd.edu>
2024-11-04 18:08:13 -0500

    support multiple case files in cfd

--- src/scripts/cfd.py
+++ src/scripts/cfd.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 @click.command(context_settings={"show_default": True})
-@click.argument("case")
+@click.argument("cases", nargs=-1, required=True)
 @click.option("--attacks",
               "-a",
               multiple=True,
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
               "-m",
               multiple=True,
               default=(0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6),
-              help="The mach numbers to parameterize.")
+              help="The Mach numbers to parameterize.")
 @click.option("--iterations",
               "-i",
               default=500,
@@ -29,24 +29,20 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
               "-p",
               default=9340,
               help="The port on which the TaskVine manager listens.")
-def cfd(case: str, attacks: tuple[int, ...], machs: tuple[int, ...],
-        iterations: int, port: int):
+def cfd(cases: tuple[str, ...], attacks: tuple[int, ...],
+        machs: tuple[int, ...], iterations: int, port: int):
     """Launch parameterized Ansys Fluent CFD jobs via TaskVine.
 
-    The case file must contain the vehicle mesh and four boundaries: farfield,
-    inlet, outlet, and launchvehicle. The vehicle's roll axis must coincide
-    with the x axis such that the drag force acts in the positive x direction.
+    The case file(s) must contain the vehicle mesh and four boundaries:
+    farfield, inlet, outlet, and launchvehicle. The vehicle's roll axis must
+    coincide with the x axis such that the drag force acts in the positive x
+    direction.
     """
     import ndcctools.taskvine as vine
 
     # Create the TaskVine manager.
     manager = vine.Manager(port)
-    case_vine_file = manager.declare_file(case)
     logging.info(f"TaskVine manager listening on port {port}")
-
-    # Retrieve the basename of the case file.
-    case_name = os.path.basename(case)
-    case_name = case_name.split(".", 2)[0]
 
     # Load the journal template.
     journal_file = f"data/journal.jou"
@@ -60,46 +56,56 @@ def cfd(case: str, attacks: tuple[int, ...], machs: tuple[int, ...],
     os.makedirs(output_directory, exist_ok=True)
     logger.info(f"Created output directory {output_directory}")
 
-    for attack in attacks:
-        for mach in machs:
-            name = f"{case_name}_{attack}_{mach}_{iterations}"
+    for case in cases:
+        # Retrieve the basename of the case file.
+        case_name = os.path.basename(case)
+        case_name = case_name.split(".", 2)[0]
 
-            # To induce an angle of attack, split the x and y components of the
-            # flow velocity vector. Note that we assume the vehicle's roll axis
-            # coincides with the x axis.
-            angle_of_attack = radians(attack)
-            flow_vector_x = cos(angle_of_attack)
-            flow_vector_y = sin(angle_of_attack)
+        # Expose the case file to TaskVine.
+        case_vine_file = manager.declare_file(case)
 
-            # Paramterize the journal template.
-            journal_paramaterized = journal_template.format(
-                mach=mach,
-                flow_vector_x=flow_vector_x,
-                flow_vector_y=flow_vector_y,
-                iterations=iterations)
+        # Perform CFD on a matrix of attack angles and Mach numbers.
+        for attack in attacks:
+            for mach in machs:
+                name = f"{case_name}_{attack}_{mach}_{iterations}"
 
-            # Create the task with inputs and outputs.
-            task = vine.Task(
-                f"module load ansys/2024R1; /opt/crc/a/ansys/2024R1/v241/fluent/bin/fluent 3ddp -t1 -g < journal.jou > log 2>&1"
-            )
-            journal_vine_buffer = manager.declare_buffer(journal_paramaterized)
-            axial_vine_file = manager.declare_file(
-                f"{output_directory}/{name}.axial")
-            normal_vine_file = manager.declare_file(
-                f"{output_directory}/{name}.normal")
-            log_vine_file = manager.declare_file(
-                f"{output_directory}/{name}.log")
-            task.add_input(case_vine_file, "case.cas.h5")
-            task.add_input(journal_vine_buffer, "journal.jou")
-            task.add_output(axial_vine_file, "axial.out")
-            task.add_output(normal_vine_file, "normal.out")
-            task.add_output(log_vine_file, "log")
+                # To induce an angle of attack, split the x and y components of
+                # the flow velocity vector. Note that we assume the vehicle's
+                # roll axis coincides with the x axis.
+                angle_of_attack = radians(attack)
+                flow_vector_x = cos(angle_of_attack)
+                flow_vector_y = sin(angle_of_attack)
 
-            # Submit the task to TaskVine.
-            manager.submit(task)
-            logger.info(
-                f"Submitted computational fluid dynamics task {task.id} with {case_name=} {attack=} {mach=} {iterations=}"
-            )
+                # Paramterize the journal template.
+                journal_paramaterized = journal_template.format(
+                    mach=mach,
+                    flow_vector_x=flow_vector_x,
+                    flow_vector_y=flow_vector_y,
+                    iterations=iterations)
+
+                # Create the task with inputs and outputs.
+                task = vine.Task(
+                    "module load ansys/2024R1; "
+                    "/opt/crc/a/ansys/2024R1/v241/fluent/bin/fluent "
+                    "3ddp -t1 -g < journal.jou > log 2>&1")
+                journal_vine_buffer = manager.declare_buffer(
+                    journal_paramaterized)
+                axial_vine_file = manager.declare_file(
+                    f"{output_directory}/{name}.axial")
+                normal_vine_file = manager.declare_file(
+                    f"{output_directory}/{name}.normal")
+                log_vine_file = manager.declare_file(
+                    f"{output_directory}/{name}.log")
+                task.add_input(case_vine_file, "case.cas.h5")
+                task.add_input(journal_vine_buffer, "journal.jou")
+                task.add_output(axial_vine_file, "axial.out")
+                task.add_output(normal_vine_file, "normal.out")
+                task.add_output(log_vine_file, "log")
+
+                # Submit the task to TaskVine.
+                manager.submit(task)
+                logger.info(f"Submitted task {task.id} with "
+                            f"{case_name=} {attack=} {mach=} {iterations=}")
 
     while not manager.empty():
         task = manager.wait(10)
