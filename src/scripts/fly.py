ff10047d93609fa7e09812cab3f7d1dd63cd08a9
Eamon Tracey <etracey@nd.edu>
2024-11-04 13:42:16 -0500

    shuffle script imports

--- src/scripts/fly.py
+++ src/scripts/fly.py
@@ -2,16 +2,16 @@ from typing import Optional
 
 import click
 
-from flight.flight import Flight
-
 
 @click.command(context_settings={"show_default": True})
-@click.option("log_file",
+@click.option("-l",
+              "--log_file",
               type=Optional[str],
               default=None,
               help="The path to which to write the log file")
 def fly(log_file: Optional[str]):
     """Run ACS flight software."""
+    from flight.flight import Flight
 
     flight = Flight(log_file)
     flight.run()
