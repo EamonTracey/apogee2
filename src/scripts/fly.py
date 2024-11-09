f7ab420171483a30a618e34f6b9231c44b1df0b0
eamontracey <etracey@nd.edu>
2024-11-08 19:29:00 -0500

    better naming and log handling

--- src/scripts/fly.py
+++ src/scripts/fly.py
@@ -1,19 +1,34 @@
+import logging
 from typing import Optional
 
 import click
 
+logger = logging.getLogger(__name__)
+
 
 @click.command(context_settings={"show_default": True})
-@click.option("-l",
-              "--log_file",
+@click.option("-n",
+              "--name",
               type=Optional[str],
               default=None,
-              help="The path to which to write the log file")
-def fly(log_file: Optional[str]):
+              help="The name of the flight, used to name log files.")
+def fly(name: Optional[str]):
     """Run ACS flight software."""
     from flight.flight import Flight
 
-    flight = Flight(log_file)
+    # Naming is hard.
+    utc_date = datetime.datetime.now(datetime.UTC)
+    utc_date_string = utc_date.strftime("%Y%m%d%H%M%S")
+    name = f"ACS {utc_date_string}"
+
+    # Initialize logging.
+    logging.basicConfig(
+        filename=f"{name.log}",
+        format="%(asctime)s:%(name)s:%(levelname)s:%(message)s",
+        datefmt="%Y%m%d%H%M%S",
+        level=logging.INFO)
+
+    flight = Flight(name)
     flight.run()
 
 
