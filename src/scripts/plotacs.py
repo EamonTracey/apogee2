56e82fca98bbe8a76b488e02ea2af5f1456d0bfe
zgwebner <zgwebner@gmail.com>
2024-12-04 22:52:30 -0500

    Updates. TODO: How plot?

--- src/scripts/plotacs.py
+++ src/scripts/plotacs.py
@@ -1,6 +1,6 @@
 import click
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 @click.command(context_settings={"show_default": True})
 @click.argument("filepath",            
@@ -9,13 +9,15 @@ import pandas as pd
               "--significant",
               is_flag=True,
               help="Display significant flight data.")
+@click.option("-af",
+              "--altitudefull",
+              is_flag=True,
+              help="Full-flight altitude vs. time")
 
-
-def plot(filepath, significant: bool):
+def plot(filepath, significant: bool, altitudefull: bool):
     """Process ACS flight data."""    
-    
-    if significant:    
-        print(filepath)
+    # Displays a few important data points about the flight.
+    if significant:
         df = pd.read_csv(filepath)
         timeon = df["Time"].iloc[-1]
         apogeeval = df["Altitude_BMP390"].max()  
@@ -23,4 +25,8 @@ def plot(filepath, significant: bool):
         print(f"How long as ACS on: {timeon} (s)")
         print(f"Apogee: {apogeeval} (ft)")
         print(f"Loop Slip Count: {slipcount}")
+
+    # Displays a full plot of alititude vs time for the entire time ACS is on.
+    if altitudefull:
+        print("Not Yet Implemented...")
 
