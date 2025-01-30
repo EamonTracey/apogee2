ddf5149968a5b8784996b625ea2529876a619b90
zgwebner <zgwebner@gmail.com>
2025-01-30 17:14:50 -0500

    Plotter doesn't plot it gives results so renamed

--- src/scripts/results.py
+++ src/scripts/results.py
@@ -13,18 +13,14 @@ import click
 def plot(filepath, significant: bool, altitudefull: bool):
     """Plot and review ACS flight data."""
     import pandas as pd
-    import matplotlib.pyplot as plt
 
     # Displays a few important data points about the flight.
     if significant:
         df = pd.read_csv(filepath)
         timeon = df["Time"].iloc[-1]
-        apogeeval = df["Altitude_BMP390"].max()
+        apogeeval = df["Filtered_Altitude"].max()
         slipcount = df["Loop_Slip_Count"].iloc[-1]
         print(f"How long as ACS on: {timeon} (s)")
         print(f"Apogee: {apogeeval} (ft)")
         print(f"Loop Slip Count: {slipcount}")
 
-    # Displays a full plot of alititude vs time for the entire time ACS is on.
-    if altitudefull:
-        print("Not Yet Implemented...")
