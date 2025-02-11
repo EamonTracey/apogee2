ac39602a87e7a65c471d8b25b34913f0eb02368d
zgwebner <zgwebner@gmail.com>
2025-02-10 22:57:59 -0500

    Fixed error

--- src/flight/log.py
+++ src/flight/log.py
@@ -98,7 +98,7 @@ class LogComponent(Component):
         self._writer.writerow(log)
         
         # If console output is selected.
-        if results == 1:
+        if self._results == 1:
             print(log[0], log[1], log[2], log[3], log[4], log[5])
-        elif results == 2:
+        elif self._results == 2:
             print(log)
