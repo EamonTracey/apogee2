6506e8e65e548c543e838aaffe8f1d517217c49e
zgwebner <zgwebner@gmail.com>
2025-02-24 19:42:14 +0000

    bored in class so made plotter actually work

--- src/scripts/plot.py
+++ src/scripts/plot.py
@@ -1,6 +1,5 @@
 import click
 
-
 @click.command(context_settings={"show_default": True})
 @click.argument("path", type=click.STRING, nargs=-1, required=True)
 @click.option("-s",
@@ -11,57 +10,54 @@ import click
     "-a",
     "--alldata",
     is_flag=True,
-    help="Toggle to display data for entire duration ACS is on vs. only flight."
-)
+    help="Include to plot full duration ACS is on. Default is flight only.")
 @click.option("-p",
               "--plot_choice",
               type=click.Choice(
-                  ["Altitude", "Velocity", "Acceleration", "Servo"]),
-              help="Plot data vs. time.")
+                  ["Alt", "Velz", "Accelz", "Servo"]),
+              help="Plot selected data vs. time.")
+
+## TODO: Add functionality to plot multiple datasets on same plot
+## TODO: More data options as ACS algorithm improves (prediction, target, zenith, etc.)
+
 def plot(path: str, significant: bool, alldata: bool, plot_choice: str):
     """A suite of tools to plot analyze flight data post-launch."""
     import matplotlib.pyplot as plt
     import pandas as pd
-    from base.stage import Stage
 
     path = ' '.join(path)
     df = pd.read_csv(path)
 
-    keys = {
-        "Altitude": "Altitude",
-        "Velocity": "Velocity_Z",
-        "Acceleration": "Acceleration_Z",
-        "Servo": "Servo_Angle"
-    }
-
-    # Displays a few important data points about the flight.
+    # Prints to console a few important data points about the flight.
     if significant:
+        
+        time_on = df["Time"].iloc[-1]
+        apogee_val = df["Altitude"].max()
+        max_servo = df["Servo_Angle"].max()
+        if "OVERSHOOT" not in df['Stage'].values:
+            overshoot_time = 0.0
+        else:
+            overshoot_time = (df.loc[df["Stage"] == "OVERSHOOT", "Time"].max() -
+                            df.loc[df["Stage"] == "OVERSHOOT", "Time"].min())
 
-        timeon = df["Time"].iloc[-1]
-        apogeeval = df["Filtered_Altitude"].max()
-        slipcount = df["Loop_Slip_Count"].iloc[-1]
-        maxservo = df["Servo_Angle"].max()
-        overshoottime = (df.loc[df["Stage"] == Stage.OVERSHOOT, "Time"].max() -
-                         df.loc[df["Stage"] == Stage.OVERSHOOT, "Time"].min())
+        print(f"Apogee: {apogee_val} ft")
+        print(f"How long as ACS on: {time_on} sec")
+        print(f"Max Servo Angle: {max_servo} degrees")
+        print(f"Number of seconds in OVERSHOOT: {overshoot_time} s")
 
-        print(f"How long as ACS on: {timeon} (s)")
-        print(f"Apogee: {apogeeval} (ft)")
-        print(f"Loop Slip Count: {slipcount}")
-        print(f"Max Servo Angle: {maxservo} (degrees)")
-        print(f"Number of seconds in OVERSHOOT: {overshoottime} (s)")
+    # Plot entire duration of ACS being on vs. just flight
+    if not alldata:
 
-    if alldata:
-
-        indstart = df[df['Stage'] == Stage.BURN].index[0] - 3000
-        indend = df[(df['Altitude'] < df.at[indstart + 3000, 'Altitude'])
-                    & (df['Stage'] == Stage.DESCENT)].index[0] + 3000
-
+        indstart = df[df['Stage'] == "BURN"].index[0] - 300
+        indend = df[(df['Altitude'] < df.at[indstart + 300, 'Altitude'])
+                    & (df['Stage'] == "DESCENT")].index[0] + 300
         rangevals = range(indstart, indend)
 
     else:
         rangevals = range(0, df.index[-1])
 
-    if plot_choice == "Altitude":
+    # Altitude Plot 
+    if plot_choice == "Alt":
 
         plt.plot(df.iloc[rangevals, df.columns.get_loc("Time")],
                  df.iloc[rangevals, df.columns.get_loc("Altitude")],
@@ -69,9 +65,9 @@ def plot(path: str, significant: bool, alldata: bool, plot_choice: str):
         plt.xlabel("Time (s)")
         plt.ylabel("Altitude (ft)")
         plt.title("Filtered Altitude vs Time")
-        plt.show()
 
-    elif plot_choice == "Velocity":
+    # z-Velocity Plot
+    elif plot_choice == "Velz":
 
         plt.plot(df.iloc[rangevals, df.columns.get_loc("Time")],
                  df.iloc[rangevals,
@@ -80,9 +76,9 @@ def plot(path: str, significant: bool, alldata: bool, plot_choice: str):
         plt.xlabel("Time (s)")
         plt.ylabel("Vertical Velocity (ft/s)")
         plt.title("Filtered Vertical Velocity vs Time")
-        plt.show()
 
-    elif plot_choice == "Acceleration":
+    # z-Acceleration Plot
+    elif plot_choice == "Accelz":
 
         plt.plot(df.iloc[rangevals, df.columns.get_loc("Time")],
                  df.iloc[rangevals,
@@ -91,8 +87,8 @@ def plot(path: str, significant: bool, alldata: bool, plot_choice: str):
         plt.xlabel("Time (s)")
         plt.ylabel("Vertical Acceleration (ft/s^2)")
         plt.title("Filtered Vertical Acceleration vs Time")
-        plt.show()
 
+    # Servo Angle Plot
     elif plot_choice == "Servo":
 
         plt.plot(df.iloc[rangevals, df.columns.get_loc("Time")],
@@ -102,4 +98,6 @@ def plot(path: str, significant: bool, alldata: bool, plot_choice: str):
         plt.xlabel("Time (s)")
         plt.ylabel("Servo Angle (degrees)")
         plt.title("Servo Angle vs Time")
+
+    if plot_choice is not None:
         plt.show()
