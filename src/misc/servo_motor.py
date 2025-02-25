68e4114a4f6ef5e7bbc4ca332db5d8b72993865f
Eamon Tracey <etracey@nd.edu>
2025-02-25 13:52:08 -0500

    simple

--- src/misc/servo_motor.py
+++ src/misc/servo_motor.py
@@ -34,5 +34,18 @@ class ServoMotor:
 
 servo_motor = ServoMotor(board.D12)
 while True:
-    degrees = float(input("Rotation (°): "))
+    degrees = input("Rotation (°): ")
+    if degrees.lower() in ["q", "quit", "exit"]:
+        break
+
+    try:
+        degrees = float(degrees)
+    except ValueError:
+        print(f"Invalid number {degrees}.", file=sys.stderr)
+        continue
+
+    if not (0 <= degrees <= 45):
+        print(f"{degrees} not in the range 0-45.", file=sys.stderr)
+        continue
+
     servo_motor.rotate(degrees)
